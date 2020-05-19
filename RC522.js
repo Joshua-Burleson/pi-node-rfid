543ddd359c47a69949300f0d2d5cdc438df65ea0
Joshua Burleson <josh.burleson@bytes.co>
2020-05-18 21:16:46 -0400

    Initial experiment

--- RC522.js
+++ RC522.js
@@ -20,54 +20,43 @@ class RC522 extends Mfrc522 {
          this.setBuzzerPin(buzzerPin);
     }
 
-    /**
-     * Recieve and process result of card-read
-     * @callback cardReadCallback
-     */
-    /**
-     * RFID Read Mode.
-     * Set RC522 to read-mode. Define callback and checking interval.
-     * @param {cardReadCallback} callback
-     * @param {number} interval - Interval frequency (in ms)
-     */
-    readMode = (callback, interval = 500) => setInterval( () => {
+
+    readMode = ( mainThread ) => setInterval( () => {
         //# reset card
         this.reset();
 
-        //# Get the UID of the scanned card
+        //# Get the UID of the card
         const response = this.getUid();
 
-        if ( ! response.status ) {
+        if ( !response.status ) {
             // Emit scan-read error
-            callback("UID Scan Error");
+            mainThread.port.postMessage("UID Scan Error");
             return;
         }
 
         const uid = response.data.reduce( (uidCode, char) => `${uidCode} ${char.toString(16)}`, '' );
         //Emit card UID to parent thread
-        callback(`Card read UID: ${uid}`);
+        mainThread.port.postMessage(`Card read UID: ${uid}`);
 
         //# Key for authentication
         const key = this.authKey;
 
         //# Authenticate on Block 8 with key and uid
-        if ( ! this.authenticate(8, key, uid) ) {
-            callback("Authentication Error");
+        if (!this.authenticate(8, key, uid)) {
+            //mainThread.port.postMessage("Authentication Error");
             return;
         }
 
         /*
         const memCap = this.selectCard(uid);
-        //callback(`Card Memory Capacity: ${memCap}`);
+        //mainThread.port.postMessage("Card Memory Capacity: " + memoryCapacity);
         */
 
         //# Stop
         this.stopCrypto();
-        }, interval);
+        }, 500);
 
-    writeCard = () => {
-        this.reset();
-    }
+
 }
 
 module.exports = RC522;
