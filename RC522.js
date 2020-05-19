7a0375dc57e405b1eeb20137ad2f7be1c9715318
Joshua Burleson <josh.burleson@bytes.co>
2020-05-18 21:31:16 -0400

    Initial experiment

--- RC522.js
+++ RC522.js
@@ -19,6 +19,51 @@ class RC522 extends Mfrc522 {
          this.setBuzzerPin(buzzerPin);
     }
 
+    readMode = message => {
+        setInterval(function() {
+            //# reset card
+            mfrc522.reset();
+    
+            //# Scan for cards
+            let response = mfrc522.findCard();
+            if (!response.status) {
+                //console.log("No Card");
+                return;
+            }
+            //console.log("Card detected, CardType: " + response.bitSize);
+    
+            //# Get the UID of the card
+            response = mfrc522.getUid();
+            if (!response.status) {
+                //console.log("UID Scan Error");
+                return;
+            }
+            //# If we have the UID, continue
+            const uid = response.data.reduce( (uidCode, char) => `${uidCode} ${char.toString(16)}`, '' );
+            //console.log(`Card read UID: ${uid}`);
+            message.port.postMessage(`Card read UID: ${uid}`)
+    
+            //# Select the scanned card
+            const memoryCapacity = mfrc522.selectCard(uid);
+            //console.log("Card Memory Capacity: " + memoryCapacity);
+    
+            //# This is the default key for authentication
+            const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
+    
+            //# Authenticate on Block 8 with key and uid
+            if (!mfrc522.authenticate(8, key, uid)) {
+                //console.log("Authentication Error");
+                return;
+            }
+    
+            //# Dump Block 8
+            //console.log("Block: 8 Data: " + mfrc522.getDataForBlock(8));
+    
+            //# Stop
+            mfrc522.stopCrypto();
+            }, 500);
+    }
+
 
     /* readMode = ( message ) => setInterval(() => {
         //# reset card
--- worker.js
+++ worker.js
@@ -13,46 +13,5 @@ const mfrc522 = new Mfrc522();
 
 parentPort.on('message', (message) => {
     console.log(message)
-    setInterval(function() {
-        //# reset card
-        mfrc522.reset();
-
-        //# Scan for cards
-        let response = mfrc522.findCard();
-        if (!response.status) {
-            //console.log("No Card");
-            return;
-        }
-        //console.log("Card detected, CardType: " + response.bitSize);
-
-        //# Get the UID of the card
-        response = mfrc522.getUid();
-        if (!response.status) {
-            //console.log("UID Scan Error");
-            return;
-        }
-        //# If we have the UID, continue
-        const uid = response.data.reduce( (uidCode, char) => `${uidCode} ${char.toString(16)}`, '' );
-        //console.log(`Card read UID: ${uid}`);
-        message.port.postMessage(`Card read UID: ${uid}`)
-
-        //# Select the scanned card
-        const memoryCapacity = mfrc522.selectCard(uid);
-        //console.log("Card Memory Capacity: " + memoryCapacity);
-
-        //# This is the default key for authentication
-        const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
-
-        //# Authenticate on Block 8 with key and uid
-        if (!mfrc522.authenticate(8, key, uid)) {
-            //console.log("Authentication Error");
-            return;
-        }
-
-        //# Dump Block 8
-        //console.log("Block: 8 Data: " + mfrc522.getDataForBlock(8));
-
-        //# Stop
-        mfrc522.stopCrypto();
-        }, 500);
+    mfrc522.readMode(message);
 })
