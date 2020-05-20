e5cd46dd5ea3e77be0949757e27bb610daa299ca
Joshua Burleson <josh.burleson@bytes.co>
2020-05-19 23:06:57 -0400

    Attempting message wrapping callback

--- RC522.js
+++ RC522.js
@@ -21,35 +21,28 @@ class RC522 extends Mfrc522 {
     }
 
     readMode = callback =>  {
-            //# reset card
-            //# Scan for cards
-            let response = this.findCard();
-            if (!response.status) {
-                //console.log("No Card");
+            const scan = this.findCard();
+            if (!scan.status) {
                 return;
             }
             //console.log("Card detected, CardType: " + response.bitSize);
     
             //# Get the UID of the card
-            response = this.getUid();
+            const response = this.getUid();
             if (!response.status) {
                 //console.log("UID Scan Error");
                 return;
             }
             //# If we have the UID, continue
             const uid = response.data.reduce( (uidCode, char) => `${uidCode} ${char.toString(16)}`, '' );
-            //console.log(`Card read UID: ${uid}`);
-            callback(`Card read UID: ${uid}`)
+            callback(`Card read UID: ${uid}`);
     
             //# Select the scanned card
             const memoryCapacity = this.selectCard(uid);
             //console.log("Card Memory Capacity: " + memoryCapacity);
     
-            //# This is the default key for authentication
-            const key = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
-    
             //# Authenticate on Block 8 with key and uid
-            if (!this.authenticate(8, key, uid)) {
+            if (!this.authenticate(8, this.authKeykey, uid)) {
                 //console.log("Authentication Error");
                 return;
             }
@@ -75,51 +68,6 @@ class RC522 extends Mfrc522 {
     }
 
     #init = (operation, interval, callback) => this.activeOperation = setInterval(operation, interval, callback);
-
-
-    /* readMode = ( message ) => setInterval(() => {
-        //# reset card
-        this.reset();
-
-        //# Scan for cards
-        const scanResponse = this.findCard();
-        if ( !scanResponse.status ) {
-            //console.log("No Card");
-            return;
-        }
-        //console.log("Card detected, CardType: " + response.bitSize);
-
-        //# Get the UID of the card
-        const response = this.getUid();
-        if (!response.status) {
-            //console.log("UID Scan Error");
-            return;
-        }
-        //# If we have the UID, continue
-        const uid = response.data.reduce( (uidCode, char) => `${uidCode} ${char.toString(16)}`, '' );
-        //console.log(`Card read UID: ${uid}`);
-        message.port.postMessage(`Card read UID: ${uid}`)
-
-        //# Select the scanned card
-        const memoryCapacity = this.selectCard(uid);
-        //console.log("Card Memory Capacity: " + memoryCapacity);
-
-        //# This is the default key for authentication
-        const key = this.authKey;
-
-        //# Authenticate on Block 8 with key and uid
-        if (!this.authenticate(8, key, uid)) {
-            //console.log("Authentication Error");
-            return;
-        }
-
-        //# Dump Block 8
-        //console.log("Block: 8 Data: " + this.getDataForBlock(8));
-
-        //# Stop
-        this.stopCrypto();
-        }, 500); */
-
 
 }
 
--- worker.js
+++ worker.js
@@ -1,17 +1,10 @@
 "use strict";
-const Mfrc522 = require("./RC522");
 const { parentPort } = require('worker_threads');
+const Mfrc522 = require("./RC522");
 
-//# This loop keeps checking for chips. If one is near it will get the UID and authenticate
-//console.log("scanning...");
-//console.log("Please put chip or keycard in the antenna inductive zone!");
-//console.log("Press Ctrl-C to stop.");
-
-// GPIO 24 can be used for buzzer bin (PIN 18), Reset pin is (PIN 22).
-// I believe that channing pattern is better for configuring pins which are optional methods to use.
-const mfrc522 = new Mfrc522();
+const RFIDReader = new Mfrc522();
 
 parentPort.on('message', (message) => {
     const reply = (res) => message.port.postMessage(res);
-    mfrc522.runReadMode(reply);
-})
+    RFIDReader.runReadMode(reply);
+});
