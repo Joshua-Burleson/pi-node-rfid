660c82e8a14e45fd9b299942158e345afd3d10ee
Joshua Burleson <josh.burleson@bytes.co>
2020-05-25 03:19:57 -0400

    .

--- RC522.js
+++ RC522.js
@@ -13,17 +13,24 @@ const defaultAuthKey = [0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
 
 class RC522 extends Mfrc522 {
 
-   constructor( {authKey = defaultAuthKey, spiSettings = defaultSPI, resetPin = 22, buzzerPin = 18} = {} ){
+   constructor( {authKey = defaultAuthKey, authBlock= 10, spiSettings = defaultSPI, resetPin = 22, buzzerPin = 18} = {} ){
          super(spiSettings);
         //  Constant
          this.authKey = authKey;
+         this.authBlock = authBlock;
          this.setResetPin(resetPin);
          this.setBuzzerPin(buzzerPin);
         //  Stateful
          this.activeOperation = null;
+         this.interruptable = {
+             operation: null,
+             callback: null,
+             interval: null
+         };
          this.faultCount = 0;
     }
 
+    // Read Modes
     readMode = callback =>  {
         const cardData = {
             uid: null,
@@ -49,6 +56,7 @@ class RC522 extends Mfrc522 {
             cardData.uid = response.data.reduce( (uidCode, char, index) => `${uidCode.concat(char.toString(16))}${index === response.data.length - 1 ? '' : ' '}`, '' );
             cardData.memory_capacity = this.selectCard(response.data);
             cardData.auth = this.authenticate(8, this.authKey, response.data);
+            
             this.stopCrypto();
             callback(cardData);
             this.faultCount = 0;
@@ -58,7 +66,7 @@ class RC522 extends Mfrc522 {
             callback({
                 ...cardData,
                 read_error: true,
-                error_message: err,
+                message: err,
                 concurrent_fault_count: this.faultCount
             });
             console.error('An unhandled error has occured in read mode. Restarting read process at default interval of 500ms.');
@@ -66,16 +74,56 @@ class RC522 extends Mfrc522 {
         }
     }
 
+    // Write modes
+    writeAuthenticationKey( newKey, callback, address = this.authBlock ){
+        const result = {
+            newKey: null,
+            write_error: true,
+            message: null
+        };
+        try {
+            this.reset();
+            // Validation
+            if (!address || !newKey) {
+                return callback({...result, write_error: true, message: 'Ensure address-block and new key are defined'});
+            }
+            if (address % 4 !== 3) {
+                const offset = 3 - (address % 4);
+                return callback({
+                    ...result,
+                    write_error: true,
+                    message: `Error: Chosen block is not a sector trailer! Please write authentication key to block ${address + offset}`
+                    });
+            }
+            if (newKey.length !== 6) {
+                return callback({
+                    ...result,
+                    write_error: true,
+                    message: `Error: Key length must be 6`
+                });
+            }
+            // Write new key
+            const newData = newKey.concat(this.getDataForBlock(address).slice(6));
+            callback( this.writeDataToBlock(address, newData) );
+            return this.interruptable ? this.#init( ...Object.values(this.interruptable) ) : newKey;
+        } catch (err) {
+            console.error(err);
+            callback( {...result, write_error: true, message: err} );
+        }
+    }
+
+    // User-Friendlier Methods
+    runReadMode = (callback, interval = 500) => this.#init(this.readMode, interval, callback);
+
+    // Object Utility Methods
     reset = () => {
         super.reset();
         clearInterval(this.activeOperation);
         this.activeOperation = null;
     }
-
-    runReadMode = (callback, interval = 500) => this.#init(this.readMode, interval, callback);
-
     #init = (operation, interval, callback) => {
         this.reset();
+        this.interruptable = { operation, callback, interval };
         this.activeOperation = setInterval(operation, interval, callback);
     }
 
